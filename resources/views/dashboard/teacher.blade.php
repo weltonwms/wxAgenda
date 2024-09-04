12be5bd3c8fe129c8fce19b3a5a7896c751da98a
Welton Santos <weltonwms@gmail.com>
2024-09-04 13:09:36 -0300

    link bot telegram teacher

--- resources/views/dashboard/teacher.blade.php
+++ resources/views/dashboard/teacher.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">Link Teacher  Drive</div>
 
@@ -8,7 +8,25 @@
                    <a href="{{env('TEACHER_LINK_DRIVE', '#')}}" target="_blank">
                    {{env('TEACHER_LINK_DRIVE', '')}}
                    </a>
-                   <br><br>
+                   <br><br><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">BOT do TELEGRAM</div>
+
+                <div class="card-body">
+                   <a href="http://t.me/idiomClub_bot" target="_blanck">
+                   http://t.me/idiomClub_bot
+                   </a>
+                   <p> 
+                    <small class="text-muted">
+                    Se não quiser clicar no link, 
+                    você pode procurar direto no telegram por : idiomClub_bot
+                    </small>
+                    </p>
                 </div>
             </div>
         </div>
