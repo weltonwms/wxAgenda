3bd455b7b09b515659abfa752195d1f2b7152e6e
Welton Santos <weltonwms@gmail.com>
2025-05-22 12:22:53 -0300

    link aulas gravadas

--- resources/views/dashboard/teacher.blade.php
+++ resources/views/dashboard/teacher.blade.php
@@ -2,13 +2,13 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card">
-                <div class="card-header">Link Teacher  Drive</div>
+                <div class="card-header">Link Teacher Drive</div>
 
                 <div class="card-body">
-                   <a href="{{env('TEACHER_LINK_DRIVE', '#')}}" target="_blank">
-                   {{env('TEACHER_LINK_DRIVE', '')}}
-                   </a>
-                   <br><br><br>
+                    <a href="{{env('TEACHER_LINK_DRIVE', '#')}}" target="_blank">
+                        {{env('TEACHER_LINK_DRIVE', '')}}
+                    </a>
+                    <br><br><br>
                 </div>
             </div>
         </div>
@@ -18,20 +18,38 @@
                 <div class="card-header">BOT do TELEGRAM</div>
 
                 <div class="card-body">
-                   <a href="http://t.me/idiomClub_bot" target="_blanck">
-                   http://t.me/idiomClub_bot
-                   </a>
-                   <p> 
-                    <small class="text-muted">
-                    Se não quiser clicar no link, 
-                    você pode procurar direto no telegram por : idiomClub_bot
-                    </small>
+                    <a href="http://t.me/idiomClub_bot" target="_blanck">
+                        http://t.me/idiomClub_bot
+                    </a>
+                    <p>
+                        <small class="text-muted">
+                            Se não quiser clicar no link,
+                            você pode procurar direto no telegram por : idiomClub_bot
+                        </small>
                     </p>
                 </div>
             </div>
         </div>
-
-       
-
     </div>
+
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Aulas Gravadas
+                      <i class="fa fa-camera" aria-hidden="true"></i>
+                </div>
+                <div class="card-body">
+
+                    <a href="https://1drv.ms/f/c/fbeafb609593c57a/EnrFk5Vg--oggPunTQAAAAABdEfi_M769b7URsVavkDIHg?e=KTDelf"
+                        target="_blanck">
+                        <i class="fa fa-camera" aria-hidden="true"></i>
+                        https://1drv.ms/f/c/fbeafb609593c57a/EnrFk5Vg--oggPunTQAAAAABdEfi_M769b7URsVavkDIHg?e=KTDelf
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
